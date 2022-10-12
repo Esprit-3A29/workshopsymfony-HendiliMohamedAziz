50ee720e4ad8b43dc3b9fe4344c61626d5ad7c2b
HendiliMohamedAziz <aziz3.hendili@gmail.com>
2022-10-12 18:43:47 +0100

    entity classroom,CRUD classroomContoller, form classroom

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -2,16 +2,85 @@
 
 namespace App\Controller;
 
+use App\Entity\Student;
+use App\Form\StudentType;
 use App\Repository\StudentRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class StudentController extends AbstractController
 {
-    #[Route('/students', name: 'app_student')]
-    public function ListEtudiant(StudentRepository $repository)
+    #[Route('/student', name: 'app_student')]
+    public function index(): Response
+    {
+        return $this->render('student/index.html.twig', [
+            'controller_name' => 'StudentController',
+        ]);
+    }
+
+    #[Route('/listStudent', name: 'list_student')]
+    public function listStudent(StudentRepository $repository)
     {
         $students= $repository->findAll();
-        return $this->render("etudiant/listEtudiant.html.twig",array("tabStudent"=>$students));
+       // $students= $this->getDoctrine()->getRepository(StudentRepository::class)->findAll();
+       return $this->render("student/list.html.twig",array("tabStudent"=>$students));
+    }
+
+
+    #[Route('/addStudent', name: 'add_student')]
+    public function addStudent(ManagerRegistry $doctrine)
+    {
+        $student= new Student();
+        $student->setRef("258");
+        $student->setUsername("rahma");
+        $student->setMoyenne(18);
+       // $em=$this->getDoctrine()->getManager();
+        $em= $doctrine->getManager();
+        $em->persist($student);
+        $em->flush();
+        return $this->redirectToRoute("list_student");
+    }
+
+    #[Route('/addForm', name: 'add2')]
+    public function addForm(ManagerRegistry $doctrine,Request $request)
+    {
+        $student= new Student;
+        $form= $this->createForm(StudentType::class,$student);
+        $form->handleRequest($request) ;
+        if ($form->isSubmitted()){
+             $em= $doctrine->getManager();
+             $em->persist($student);
+             $em->flush();
+             return  $this->redirectToRoute("list_student");
+         }
+        return $this->renderForm("student/add.html.twig",array("formStudent"=>$form));
+    }
+
+    #[Route('/updateForm/{ref}', name: 'update')]
+    public function  updateForm($ref,StudentRepository $repository,ManagerRegistry $doctrine,Request $request)
+    {
+        $student= $repository->find($ref);
+        $form= $this->createForm(StudentType::class,$student);
+        $form->handleRequest($request) ;
+        if ($form->isSubmitted()){
+            $em= $doctrine->getManager();
+            $em->flush();
+            return  $this->redirectToRoute("list_student");
+        }
+        return $this->renderForm("student/update.html.twig",array("formStudent"=>$form));
+    }
+
+    #[Route('/removeForm/{ref}', name: 'remove')]
+
+    public function removeStudent(ManagerRegistry $doctrine,$ref,StudentRepository $repository)
+    {
+        $student= $repository->find($ref);
+        $em = $doctrine->getManager();
+        $em->remove($student);
+        $em->flush();
+        return  $this->redirectToRoute("list_student");
     }
 }
